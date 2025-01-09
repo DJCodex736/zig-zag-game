6f6335089e066f30d5c9b6e5f03c438ebe442f29
Daniel Jose <gautama831@gmail.com>
2025-01-09 16:44:39 -0300

    Add files via upload
    Pequenas melhorias de forma a deixar as coisas mais divertidas. Contem um prototipo de contagem de pontos.

--- track.js
+++ track.js
@@ -1,60 +1,72 @@
 class Track {
-   constructor(left, top, num, shortSide, longSide) {
-      this.left = left;
-      this.top = top;
-      this.shortSide = shortSide;
-      this.longSide = longSide;
-
-      this.rectangles = [];
-      this.index = 0;
-      while(this.index < num) {
-         this.enqueueRectangle();
-      }
-   }
-
-   enqueueRectangle() {
-      if (this.index % 2 == 0) {
-         const [width, height] = [this.longSide, this.shortSide];
-         this.rectangles.push(new Rectangle(this.left, this.top, width, height));
-         this.left += width - this.shortSide;
-      } else {
-         const [width, height] = [this.shortSide, this.longSide];
-         this.rectangles.push(new Rectangle(this.left, this.top, width, height));
-         this.top += height - this.shortSide;
-      }
-      this.index ++;
-   }
-
-   dequeueRectangle() {
-      this.rectangles.shift();
-   }
-
-   adjustFor(player) {
-      for (let i = 0; i < this.rectangles.length; i++) {
-         const rect = this.rectangles[i];
-         if (rect.contains(player)) {
-            if (i > this.rectangles.length / 2) {
-               this.dequeueRectangle();
-               this.enqueueRectangle();
-               return;
-            }
-         }
-      }
-   }
-
-   contains(player) {
-      let isOnTrack = false;
-      for (const rect of this.rectangles) {
-         if (rect.contains(player)) {
+    constructor(left, top, num, shortSide, longSide) {
+       this.left = left;
+       this.top = top;
+       this.shortSide = shortSide;
+       this.longSide = longSide;
+       this.score = 0;
+ 
+       this.rectangles = [];
+       this.index = 0;
+       while(this.index < num) {
+          this.enqueueRectangle();
+       }
+    }
+ 
+    enqueueRectangle() {
+        let adj = (this.longSide - this.shortSide) * Math.random();
+        if (this.index % 2 == 0) {
+            const [width, height] = [this.longSide + adj, this.shortSide];
+            this.rectangles.push(new Rectangle(this.left, this.top, width, height));
+            this.left += width - this.shortSide;
+        } else {
+          const [width, height] = [this.shortSide, this.longSide + adj];
+          this.rectangles.push(new Rectangle(this.left, this.top, width, height));
+          this.top += height - this.shortSide;
+       }
+       this.index ++;
+    }
+    
+    dequeueRectangle() {
+        this.rectangles.shift();
+    }
+    
+    adjustFor(player) {
+        for (let i = 0; i < this.rectangles.length; i++) {
+            const rect = this.rectangles[i];
+            if (rect.contains(player)) {
+                if (i > this.rectangles.length / 2) {
+                    this.dequeueRectangle();
+                    this.enqueueRectangle();
+// I like to use prime numbers to avoid getting too predictable :-)
+                    if (this.score % 11 == 0)
+                        player.moreSpeed();
+                    if (this.index % 7 == 0)
+                        player.moreSpeed();
+                    else if (this.index % 13 == 0)
+                        player.lessSpeed();
+                return;
+             }
+          }
+       }
+    }
+ 
+    contains(player) {
+       let isOnTrack = false;
+       for (const rect of this.rectangles) {
+          if (rect.contains(player)) {
+                if (rect.isScore(player))
+                    this.score++;
             isOnTrack = true;
-         }
-      }
-      return isOnTrack;
-   }
-
-   draw(ctx) {
-      for (const rect of this.rectangles) {
-         rect.draw(ctx);
-      }
-   }
-}
+            break;
+          }
+       }
+       return isOnTrack;
+    }
+ 
+    draw(ctx) {
+       for (const rect of this.rectangles) {
+            rect.draw(ctx);
+       }
+    }
+ }
